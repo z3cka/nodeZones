de0edec626cd6a3b8f486045d8f48f25af8fe98d
Casey Grzecka <c@sey.gr>
2013-08-07 10:05:42 -0700

    fixing comments to match my code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var gpio = require("pi-gpio");
 
-gpio.open(7, "output", function(err) {        // Open pin 16 for output
-    gpio.write(7, 0, function() {            // Set pin 16 high (1)
-        gpio.close(7);                        // Close pin 16
+gpio.open(7, "output", function(err) {        // Open pin 7 for output
+    gpio.write(7, 0, function() {            // Set pin 7 high (1)
+        gpio.close(7);                        // Close pin 7
     });
 });
 
