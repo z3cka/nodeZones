8ba51c10ad1690128b29d440a590456aa5ed8ee6
Casey Grzecka <c@sey.gr>
2013-08-08 23:51:29 -0700

    added ability to turn LED on and off and different colors with keys

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 var gpio = require("pi-gpio");
+var keypress = require('keypress');
 
-gpio.open(7, "output", function(err) {        // Open pin 16 for output
-    gpio.write(7, 0, function() {            // Set pin 16 high (1)
-        gpio.close(7);                        // Close pin 16
-    });
+keypress(process.stdin);
+process.stdin.resume(); 
+process.stdin.setEncoding('utf8'); 
+process.stdin.setRawMode(true); 
+
+process.stdin.on('keypress', function (ch, key) {
+   
+  if ( !key ) return;
+
+  if ( key.name == 'q' ) {
+    console.log('Quitting');
+    process.exit();
+
+  } else if ( key.name == 'g' ) {
+    lightSwitch(7, 1);
+    console.log('Green On');
+
+  } else if ( key.name == 'h' ) {
+    lightSwitch(7, 0);
+    console.log('Green Off');
+
+  } else if ( key.name == 'b' ) {
+    lightSwitch(11, 1);
+    console.log('Blue On');
+
+  } else if ( key.name == 'n' ) {
+    lightSwitch(11, 0);
+    console.log('Blue Off');
+
+  } else if ( key.name == 'r' ) {
+    lightSwitch(12, 1);
+    console.log('Red On');
+
+  } else if ( key.name == 't' ) {
+    lightSwitch(12, 0);
+    console.log('Red Off');
+
+  }
 });
 
+// turn LED on and off  
+function lightSwitch(pin, state) {
+  gpio.open(pin, "output", function(err) {
+    gpio.write(pin, state, function() { 
+      gpio.close(pin);
+    });
+  });
+}
